e6c9b2ba26be8b3f50a0729fdccb20c4a8b0c4fd
Alexey Yesipenko <alex7y@gmail.com>
2019-07-01 00:00:58 +0300

    new update_quality

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -3,51 +3,41 @@ class GildedRose
     @items = items
   end
 
-  def update_quality()
+  def update_quality
     @items.each do |item|
-      if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
-        if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality = item.quality - 1
-          end
-        end
+      if item.quality > 0
+        delta = quality_delta(item)
+        item.quality = quality_range(item.quality - delta) unless delta == 0
+      end
+      item.sell_in -= 1
+    end
+  end
+
+  private
+
+  def quality_range(val) [0, [50, val].min].max end
+  def quality_delta(item)
+    default = 1
+    default = 2 if item.sell_in <= 0
+    case item.name
+    when 'Aged Brie'
+      -default
+    when 'Sulfuras'
+      0
+    when 'Backstage passes to a TAFKAL80ETC concert'
+      return item.quality if item.sell_in <= 0 # reset to 0
+      case item.sell_in
+      when 6..10
+        -2
+      when 1..5
+        -3
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
+        -1
       end
-      if item.name != 'Sulfuras, Hand of Ragnaros'
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != 'Aged Brie'
-          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            if item.quality > 0
-              if item.name != 'Sulfuras, Hand of Ragnaros'
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+    when 'Conjured'
+      2 * default
+    else
+      default
     end
   end
 end
